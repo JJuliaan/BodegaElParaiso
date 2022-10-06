f1b7d6db7785e8643f6d5407864e39e0a9c3fc55
Julian <julyagueroa@gmail.com>
2022-10-05 22:54:38 -0300

    JS

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// let == declarar y/o definir
+// nombreDeLaVariable == definicion
+// const nombreDeLaVariable = tipoDeDato
+// concatenacion - nombreDeLaVariable = variable + variable
+// console.log(mensaje o variable) = imprimir en pantalla
+// alert = imprimir por pantalla en alerta
+// prompt = guardar informacion y un leer. Pedir datos. let nombreDeLaVariable = prompt(cosas)
+let nombre;
+let apellido;
+let edad;
+alert = "Ingrese su nombre, apellido y edad";
+prompt = nombre, apellido, edad;
+if(edad < 18){
+    console.log("No eres mayor de 18 años");
+} else{
+    console.log("Hola " + nombre + " " + apellido);
+};
