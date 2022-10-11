687ecad68d85ac8aa3afbce1e9f91c75e247c42a
Julian <julyagueroa@gmail.com>
2022-10-10 22:32:11 -0300

    IF

--- js/main.js
+++ js/main.js
@@ -5,13 +5,20 @@
 // console.log(mensaje o variable) = imprimir en pantalla
 // alert = imprimir por pantalla en alerta
 // prompt = guardar informacion y un leer. Pedir datos. let nombreDeLaVariable = prompt(cosas)
-let nombre;
-let apellido;
-let edad;
-alert = "Ingrese su nombre, apellido y edad";
-prompt = nombre, apellido, edad;
-if(edad < 18){
-    console.log("No eres mayor de 18 años");
-} else{
-    console.log("Hola " + nombre + " " + apellido);
-};
+// parseFloat(nombreDeLaVariable) = convierte a float
+// parseInt(nombreDeLaVariable) = le saca los decimales
+// let nombre;
+// let apellido;
+// let edad = prompt("Ingresa tu edad");
+// if(edad >= 18) {
+//     console.log("Hola Bienvenido");
+// } else {
+//     console.log("No sos mayor de edad");
+// }
+let nombre = prompt("Ingrese el nombre");
+let apellido = prompt("Ingresa tu apellido");
+if((nombre != "") && (apellido != "")){
+    console.log("Bienvenido/a " + nombre + " " + apellido);
+} else {
+    console.log("Los campos son requeridos");
+}
